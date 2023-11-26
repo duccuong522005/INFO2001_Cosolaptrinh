3962109e3316ef7f3de9296f3a2d2ff3218c0fe0
duccuong522005 <cuongducmai123@gmail.com>
2023-11-26 21:51:41 +0700

    1. Quản lý danh sách sinh viên, sắp xếp và tìm kiếm

---
+++ 1. Quản lý danh sách sinh viên, sắp xếp và tìm kiếm.cpp
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <string.h>
+
+#define STUDENTS 50 
+
+struct Student {
+    int id;
+    char name[50];
+    float score;
+};
+
+void displayStudent(struct Student student) {
+    printf("ID: %d, Name: %s, Score: %.2f\n", student.id, student.name, student.score);
+}
+
+int main() {
+    struct Student studentList[STUDENTS];
+    int count = 0;
+    int options;
+
+    do {
+        printf("1. Add Student\n2. Display Students\n3. Sort Students\n4. Search Students\n5. Exit\n");
+        printf("Enter your option: ");
+        scanf("%d", &options);
+
+        switch (options) {
+            case 1: {
+                if (count < STUDENTS) {
+                    printf("Enter ID: ");
+                    scanf("%d", &studentList[count].id);
+                    printf("Enter Name: ");
+                    scanf("%s", studentList[count].name);
+                    printf("Enter Score: ");
+                    scanf("%f", &studentList[count].score);
+                    count++;
+                } else {
+                    printf("Student list is full.\n");
+                }
+                break;
+            }
+            case 2: {
+                printf("Student List:\n");
+                for (int i = 0; i < count; ++i) {
+                    displayStudent(studentList[i]);
+                }
+                break;
+            }
+            case 3: {
+                for (int i = 0; i < count - 1; ++i) {
+                    for (int j = 0; j < count - i - 1; ++j) {
+                        if (strcmp(studentList[j].name, studentList[j + 1].name) > 0) {
+                            struct Student temp = studentList[j];
+                            studentList[j] = studentList[j + 1];
+                            studentList[j + 1] = temp;
+                        }
+                    }
+                }
+                printf("Students sorted by name.\n");
+                break;
+            }
+            case 4: {
+                char searchName[50];
+                printf("Enter Name to search: ");
+                scanf("%s", searchName);
+
+                int found = 0;
+                for (int i = 0; i < count; ++i) {
+                    if (strcmp(studentList[i].name, searchName) == 0) {
+                        printf("Student found:\n");
+                        displayStudent(studentList[i]);
+                        found = 1;
+                        break;
+                    }
+                }
+                if (!found) {
+                    printf("Student not found.\n");
+                }
+                break;
+            }
+            case 5:
+                printf("Exiting...\n");
+                break;
+            default:
+                printf("Invalid options. Please try again.\n");
+        }
+    } while (options != 5);
+
+    return 0;
+}
+
